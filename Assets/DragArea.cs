3aa8a8dc4c9f429bb05641a0dd49cb67a647c9d5
Hideki Arima <hidee@yahoo.co.jp>
2015-08-08 16:16:22 +0900

    Ground Move with Mouse Drag

--- Assets/DragArea.cs
+++ Assets/DragArea.cs
@@ -10,6 +10,10 @@ public class DragArea : MonoBehaviour
     float DragTime;
     float DragTimeDisplay;
 
+    float height = 0.0f;
+    float accelaration = 0.0f;
+
+
     // Use this for initialization
     void Start()
     {
@@ -20,14 +24,28 @@ public class DragArea : MonoBehaviour
     void Update()
     {
         DragTime += Time.deltaTime;
+        accelaration -= 0.01f;
+        height += accelaration;
+        if (height < 0.0f)
+        {
+            accelaration = 0.0f;
+            height = 0.0f;
+        }
+
+        if (height < 10.0f)
+        {
+            GameObject Ground = GameObject.Find("Ground");
+            Ground.transform.position = new Vector3(Ground.transform.position.x,  - 3.35f - height, 0);
+        }
     }
 
     void OnGUI()
     {
         GUI.Label(new Rect(20, 20, 100, 50), "Start : "  + startPosition.x.ToString() + "," + startPosition.y.ToString());
-        GUI.Label(new Rect(20, 60, 100, 50), "End   : " + endPosition.x.ToString() + "," + endPosition.y.ToString());
+        GUI.Label(new Rect(20, 40, 100, 50), "End   : " + endPosition.x.ToString() + "," + endPosition.y.ToString());
         
-        GUI.Label(new Rect(20, 100, 100, 50), "DragTime   : " + DragTimeDisplay.ToString());
+        GUI.Label(new Rect(20, 60, 100, 50), "DragTime   : " + DragTimeDisplay.ToString());
+        GUI.Label(new Rect(20, 80, 100, 50), "Accel   : " + accelaration.ToString());
     }
 
     public void OnMouseDown()
@@ -39,6 +57,7 @@ public class DragArea : MonoBehaviour
     public void OnMouseUp()
     {
         endPosition = Input.mousePosition;
+        accelaration += (((float)endPosition.y - (float)startPosition.y) / DragTime)/1000f;
         DragTimeDisplay = DragTime;
     }
 
