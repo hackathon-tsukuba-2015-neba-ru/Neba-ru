823b2ddbd565ae2b4974a39a88b76e5ca7b853f3
kengo Yokoyama <rbken.lo.ovl@gmail.com>
2015-08-08 15:19:11 +0900

    Add touch detection.

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
+	Vector3 startPosition;
+	Vector3 endPosition;
+
 	// Use this for initialization
 	void Start () {
 
@@ -12,6 +15,25 @@ public class GameManager : MonoBehaviour {
 	void Update () {
 		// comment
         // comment by windows
+		if (Input.touchCount > 0) {
+			Touch touch = Input.GetTouch(0);
+			switch (touch.phase) {
+				case TouchPhase.Began:
+					startPosition = touch.position;
+					break;
+				case TouchPhase.Moved:
+					// startPosition = touch.position;
+					break;
+				case TouchPhase.Ended:
+					endPosition = touch.position;
+					Debug.Log(endPosition);
+					break;
+			}
+		}
+	}
 
+	void OnGUI() {
+		GUI.Label(new Rect(20, 20, 100, 50), endPosition.x.ToString());
+		GUI.Label(new Rect(20, 40, 100, 50), endPosition.y.ToString());
 	}
 }
