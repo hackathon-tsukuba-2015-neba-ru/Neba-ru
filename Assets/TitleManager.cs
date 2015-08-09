e21a0fe51646c6e81774831da671d560e028227c
kengo Yokoyama <rbken.lo.ovl@gmail.com>
2015-08-09 10:57:03 +0900

    Add title canvas.

---
+++ Assets/TitleManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnClick() {
+    	Debug.Log("Button click!");
+
+    	// 非表示にする
+    	gameObject.SetActive(false);
+  	}
+}
